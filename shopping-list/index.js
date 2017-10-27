4639dc4aa10d79d63c91a1c7e1af05e4f89b7945
Alex Webb <alexwebbb@yahoo.com>
2017-10-27 00:01:12 +0000

    small progress

--- shopping-list/index.js
+++ shopping-list/index.js
@@ -1,3 +1,31 @@
-// your code here! 
-// and remember you'll need to link to this file
-// in a <script> element in index.html
+const myCoolFunction = () => {
+    
+    // in the style of C#, I will place private functions at top, 
+    // and public at bottom. event listeners at the very bottom
+    
+    // input type is string, return type is jquery object
+    const _itemMaker = function(item) {
+        const itemElement = 
+            $(`<li>
+                <span class="shopping-item">${item}</span>
+                <div class="shopping-item-controls">
+                  <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                  </button>
+                  <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                  </button>
+                </div>
+            </li>`);
+        
+        return itemElement;
+    } 
+    
+    $('#js-shopping-list-form').on('submit', function( event ) {
+       event.preventDefault();
+       console.log('hellllllo');
+    });
+    
+}
+
+$(myCoolFunction);
