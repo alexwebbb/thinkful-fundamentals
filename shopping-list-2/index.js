45e7025f41e24d8c1d7e67acd0824cd41ce27d0d
Alex Webb <alexwebbb@yahoo.com>
2017-10-30 16:37:03 +0000

    handle new item submit down

--- shopping-list-2/index.js
+++ shopping-list-2/index.js
@@ -7,14 +7,34 @@ const STORE = [
 
 function renderShoppingList() {
   // render the shopping list in the DOM
-  console.log('`renderShoppingList` ran');
+  console.log(STORE);
 }
 
 
 function handleNewItemSubmit() {
   // listen for users adding a new shopping list item, then add
   // to list and render list 
-  console.log('`handleNewItemSubmit` ran');
+  
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    
+    let field = $(this).find('.js-shopping-list-entry');
+    
+    if(/([A-z])\w/.test(field.val())) {
+      STORE.push(
+        {
+          name: field.val(), 
+          checked: false
+        });
+      field.val('');
+      
+      console.log('submit clicked');
+    }
+    
+  });
+  
+  renderShoppingList();
+  console.log(`handleNewItemSubmit ran`);
 }
 
 
