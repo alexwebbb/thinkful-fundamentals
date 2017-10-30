f6c29e6823853ac7a08fcab297016d7180197f1e
Alex Webb <alexwebbb@protonmail.com>
2017-10-29 21:57:39 -0500

    intiialize shopping list 2

---
+++ shopping-list-2/index.js
@@ -0,0 +1,41 @@
+const STORE = [
+  {name: "apples", checked: false},
+  {name: "oranges", checked: false},
+  {name: "milk", checked: true},
+  {name: "bread", checked: false}
+];
+
+function renderShoppingList() {
+  // render the shopping list in the DOM
+  console.log('`renderShoppingList` ran');
+}
+
+
+function handleNewItemSubmit() {
+  // listen for users adding a new shopping list item, then add
+  // to list and render list 
+  console.log('`handleNewItemSubmit` ran');
+}
+
+
+function handleItemCheckClicked() {
+  // listen for users checking/unchecking list items, and
+  // render them checked/unchecked accordingly
+  console.log('`handleItemCheckClicked` ran');
+}
+
+
+function handleDeleteItemClicked() {
+  // Listen for when users want to delete an item and 
+  // delete it
+  console.log('`handleDeleteItemClicked` ran')
+}
+
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+}
+
+$(handleShoppingList);
