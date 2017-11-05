b062d123e7e575131ba8beae4cee068297e8cb57
Alex Webb <alexwebbb@yahoo.com>
2017-11-05 21:44:27 +0000

    refactoring, fixed centring

--- quiz/index.js
+++ quiz/index.js
@@ -40,7 +40,8 @@ const QUESTIONS = [{
         // this will be filled in a bit later...
         // may also be changed to a data structure 
         // that has an object for each image
-        image: null
+        image: "http://pngimg.com/uploads/building/building_PNG91.png",
+        obstacleNum: 20
     },
     {
         ID: 1,
@@ -48,7 +49,7 @@ const QUESTIONS = [{
         text: 'What is 2 + 2?',
         answers: [{
                 ID: 0,
-                value: '7',
+                value: '11',
                 position: 0,
                 offset: { left: 0, top: 0 },
                 isCorrect: false
@@ -62,14 +63,14 @@ const QUESTIONS = [{
             },
             {
                 ID: 2,
-                value: '2',
+                value: '6',
                 position: 2,
                 offset: { left: 0, top: 0 },
                 isCorrect: false
             },
             {
                 ID: 3,
-                value: '14',
+                value: '1.5',
                 position: 3,
                 offset: { left: 0, top: 0 },
                 isCorrect: false
@@ -77,7 +78,46 @@ const QUESTIONS = [{
         ],
         isAnswered: false,
         isCorrect: false,
-        image: null
+        image: null,
+        obstacleNum: 30
+    },
+    {
+        ID: 2,
+        title: 'Question 3',
+        text: 'What is 3 + 3?',
+        answers: [{
+                ID: 0,
+                value: '44',
+                position: 0,
+                offset: { left: 0, top: 0 },
+                isCorrect: false
+            },
+            {
+                ID: 1,
+                value: '-3',
+                position: 1,
+                offset: { left: 0, top: 0 },
+                isCorrect: true
+            },
+            {
+                ID: 2,
+                value: '16',
+                position: 2,
+                offset: { left: 0, top: 0 },
+                isCorrect: false
+            },
+            {
+                ID: 3,
+                value: '9',
+                position: 3,
+                offset: { left: 0, top: 0 },
+                isCorrect: false
+            }
+        ],
+        isAnswered: false,
+        isCorrect: false,
+        image: null,
+        obstacleNum: 30
     }
 ];
 
@@ -93,22 +133,12 @@ const QUESTIONS = [{
 
 
 
+function _getRandomRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-
-
-function _renderForm(index) {
-
-	const q = QUESTIONS[index];
-
-	const a = q.answers;
-
-	const formElements = [];
-
-	for (let i = 0, x = a.length; i < x; i++) {
-		
-		a[i].value;
-
-		formElements.push(`
+function _generateAnswer(a, q, i) {
+    return `
 				<div
 					style="position: absolute;"
 					>
@@ -130,10 +160,52 @@ function _renderForm(index) {
 							id="" 
 							class="js-input-${a[i].position} 
 							value="${a[i].value}"
-						>
+						    >
 					</figure>
 				</div>				
-			`);
+			`;
+}
+
+function _generateObstruction(left, top) {
+    return `
+	        <div
+	            style="position: absolute;"
+	            >
+	            <figure 
+	                class="obstruction"
+	                style="
+				        left: ${left}px; 
+						top: ${top}px;"
+	                    >
+				    <img 
+				        src="http://pngimg.com/uploads/building/building_PNG91.png" 
+				        alt="Perhaps there is an answer behind this building..." 
+				        class="obstruct-img"
+				        >
+			    </figure>
+	        </div>
+	        `;
+}
+
+function _renderForm(index) {
+
+	const q = QUESTIONS[index];
+
+	const a = q.answers;
+
+	const formElements = [];
+	
+	for (let i = 0, x = a.length; i < x; i++) {
+
+		formElements.push(_generateAnswer(a, q, i));
+	}
+	
+	for (let j = 0, y = q.obstacleNum + a.length; j < y; j++) {
+	
+	    formElements.push(
+	        _generateObstruction(
+	            _getRandomRange(5, 760), 
+	            _getRandomRange(5, 350)));
 	}
 
 
@@ -141,30 +213,35 @@ function _renderForm(index) {
 			
 			<form 
 				method="post"
+				id="main-form"
 				>
-				<fieldset
-					>
-					<legend>
-						${q.title}
-					</legend>
-					<h1>
-						${q.text}
-					</h1>
-					<div id="drag-zone-overflow"
-						>
-						<div
-							id="drag-zone-parent"
-							>
-							<section 
-								id="drag-zone"
-								>
-								${formElements.join('')}	
-							</section>
-						</div>
-					</div>
+			    <fieldset>
+				    <legend>
+				    	${q.title}
+				    </legend>
+				    <h1>
+				    	${q.text}
+				    </h1>
+				    <div id="drag-zone-overflow"
+				    	>
+				    	<div
+				    		id="drag-zone-parent"
+				    		>
+				    		<section 
+				    			id="drag-zone"
+				    			>
+				    			${formElements.join('')}	
+				    		</section>
+				    	</div>
+				    </div>
 				</fieldset>
-			</form>			
-
+				<nav role="navigation">
+                    <span class="nav-element">Correct: </span>
+                    <span class="nav-element">Wrong: </span>
+                    <span class="nav-element" role="button">Prev</span>
+                    <span class="nav-element" role="button">Next</span>
+                </nav>
+			</form>
 		`));
 
 	return true;
@@ -175,7 +252,8 @@ function loadInitialState() {
 	_renderForm(0);
 
 	$( "#drag-zone" ).draggable({ containment: "#drag-zone-parent", scroll: false });
-	$( ".answer" ).draggable({ containment: "#drag-zone", scroll: false });
+	// $( ".answer" ).draggable({ containment: "#drag-zone", scroll: false });
+	$( ".obstruction" ).draggable({ containment: "#drag-zone", scroll: false });
 }
 
 function handleQuiz () {
